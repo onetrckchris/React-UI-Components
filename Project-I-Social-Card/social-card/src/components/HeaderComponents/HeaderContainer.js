5f9ae1303c4baa74e38ce885249fee3763a5633c
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-28 13:04:17 -0700

    completed HeaderContainer.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    const headerTitleProps = {
+        h1Text: 'Lambda School',
+        spanText: '@LambdaSchool • 28 May'
+    }
+
+    return (
+        <div>
+            <div>
+                <ImageThumbnail src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+                <HeaderTitle headerTitleProps={headerTitleProps} />
+                <HeaderContent text="Let's learn React by building simple interfaces with components. 
+                Don't try to overthink it, just keep it simple and fun. 
+                Once you feel comfortable using components you are well on your way to mastering React!" />
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <h3>
+            {props.headerTitleProps.h1Text}
+            <span>{props.headerTitleProps.spanText}</span>
+        </h3>
+    )
+}
+
+export default HeaderTitle;
