c5f4b2fdfd29ec3baa10995c31b8264df8d8ac1e
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-28 22:04:58 -0700

    completed finishing touches for styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,14 +3,109 @@ import './App.css';
 
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
+  const numberOne = {
+    buttonStyle: 'number-button',
+    value: 1
+  }
+
+  const numberTwo = {
+    buttonStyle: 'number-button',
+    value: 2
+  }
+
+  const numberThree = {
+    buttonStyle: 'number-button',
+    value: 3
+  }
+
+  const numberFour = {
+    buttonStyle: 'number-button',
+    value: 4
+  }
+
+  const numberFive = {
+    buttonStyle: 'number-button',
+    value: 5
+  }
+
+  const numberSix = {
+    buttonStyle: 'number-button',
+    value: 6
+  }
+
+  const numberSeven = {
+    buttonStyle: 'number-button',
+    value: 7
+  }
+
+  const numberEight = {
+    buttonStyle: 'number-button',
+    value: 8
+  }
+
+  const numberNine = {
+    buttonStyle: 'number-button',
+    value: 9
+  }
+
+  const divisionOperation = {
+    buttonStyle: 'operation-button',
+    value: '÷'
+  }
+
+  const multiplicationOperation = {
+    buttonStyle: 'operation-button',
+    value: 'x'
+  }
+
+  const subtractionOperation = {
+    buttonStyle: 'operation-button',
+    value: '-'
+  }
+
+  const additionOperation = {
+    buttonStyle: 'operation-button',
+    value: '+'
+  }
+
+  const resultOperation = {
+    buttonStyle: 'operation-button',
+    value: '='
+  }
+
   return (
     <div>
       <CalculatorDisplay total={0} />
-      <div className="buttons-container">
-        <ActionButton value="clear" />
-        <ActionButton value="0" />
+      <div className="content-container">
+        <div className="buttons-container">
+          <ActionButton value="clear" />
+          <div className="row1">
+            <NumberButton numberButtonProps={numberSeven} />
+            <NumberButton numberButtonProps={numberEight} />
+            <NumberButton numberButtonProps={numberNine} />
+          </div>
+          <div className="row2">
+            <NumberButton numberButtonProps={numberFour} />
+            <NumberButton numberButtonProps={numberFive} />
+            <NumberButton numberButtonProps={numberSix} />
+          </div>
+          <div className="row3">
+            <NumberButton numberButtonProps={numberOne} />
+            <NumberButton numberButtonProps={numberTwo} />
+            <NumberButton numberButtonProps={numberThree} />
+          </div>
+          <ActionButton value="0" />
+        </div>
+        <div className="operation-buttons-container">
+          <NumberButton numberButtonProps={divisionOperation} />
+          <NumberButton numberButtonProps={multiplicationOperation} />
+          <NumberButton numberButtonProps={subtractionOperation} />
+          <NumberButton numberButtonProps={additionOperation} />
+          <NumberButton numberButtonProps={resultOperation} />
+        </div>
       </div>
     </div>
   );
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import './Button.css';
 
+const NumberButton = props => {
+    function handleClick(event) {
+        event.preventDefault();
+        console.log(this);
+    }
+
+    return <div className={props.numberButtonProps.buttonStyle} onClick={handleClick}>{props.numberButtonProps.value}</div>
+}
+
+export default NumberButton;
